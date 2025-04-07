1a2839d331819e63f49cf5e5247e9ec4462e809d
yahav20 <yahavezer256@gmail.com>
2025-04-07 12:58:21 +0300

    Refactor main.py to improve code readability by adding comments for clarity on source and destination file paths and API instantiation.

--- Schedule-King/main.py
+++ Schedule-King/main.py
@@ -1,7 +1,14 @@
-from src.api.schedule_api import ScheduleAPI
+from src.api.schedule_api import ScheduleAPI  # Import the ScheduleAPI class from the schedule_api module
 
 if __name__ == "__main__":
+    # Define the default source file path for input data
     DEFAULT_SOURCE = r"C:\Users\User\Documents\Projects\Schedule-King\Software-for-building-a-student-study-schedule\Schedule-King\tests\test_files\input_test_api.txt"
+    
+    # Define the default destination file path for output data
     DEFAULT_DESTINATION = r"C:\Users\User\Documents\Projects\Schedule-King\Software-for-building-a-student-study-schedule\Schedule-King\tests\test_files\output.txt"
+    
+    # Create an instance of ScheduleAPI with the default source and destination paths
     api = ScheduleAPI(DEFAULT_SOURCE, DEFAULT_DESTINATION)
+    
+    # Call the process method to execute the main functionality of the API
     api.process()
