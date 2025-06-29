fee5ab52f44279cf9e112f7140a76175d52995d6
idorozin <rozin.ido@gmail.com>
2025-06-29 18:25:36 +0300

    feat: Update university selection to use Hebrew names and improve ID mapping

--- Schedule-King/src/components/choicefreak_loader_dialog.py
+++ Schedule-King/src/components/choicefreak_loader_dialog.py
@@ -9,14 +9,32 @@ class ChoiceFreakLoaderDialog(QDialog):
         self.setObjectName("ChoiceFreakLoaderDialog")
         self.setWindowTitle("Load Courses from ChoiceFreak")
         layout = QFormLayout(self)
+
+        # Mapping of university IDs to Hebrew names
+        self.university_map = {
+            "biu": "אוניברסיטת בר-אילן",
+            "tau": "אוניברסיטת תל-אביב",
+            "bgu": "אוניברסיטת בן-גוריון",
+            "tech": "הטכניון",
+            "ariel": "אוניברסיטת אריאל",
+            "huji": "האוניברסיטה העברית",
+            "hit": "המכון הטכנולוגי חולון",
+            "haifa": "אוניברסיטת חיפה"
+        }
+
+        # Reverse mapping for translation back to IDs
+        self.university_reverse_map = {v: k for k, v in self.university_map.items()}
+
         self.university_combo = QComboBox()
         self.university_combo.setObjectName("university_combo")
-        self.university_combo.addItems(["biu", "tau", "bgu", "tech", "ariel", "huji", "hit", "haifa"])
+        self.university_combo.addItems(self.university_map.values())
         layout.addRow("University:", self.university_combo)
+
         self.period_combo = QComboBox()
         self.period_combo.setObjectName("period_combo")
         self.period_combo.addItems(["2025-2", "2025-1", "2024-2", "2024-1"])
         layout.addRow("Period:", self.period_combo)
+
         buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         buttons.setObjectName("choicefreak_button_box")
         layout.addRow(buttons)
@@ -24,5 +42,7 @@ class ChoiceFreakLoaderDialog(QDialog):
         buttons.rejected.connect(self.reject)
 
     def _emit_selection(self):
-        self.selectionMade.emit(self.university_combo.currentText(), self.period_combo.currentText())
-        self.accept() 
+        # Translate Hebrew name back to ID before emitting
+        university_id = self.university_reverse_map[self.university_combo.currentText()]
+        self.selectionMade.emit(university_id, self.period_combo.currentText())
+        self.accept()
